7abefccd76755a8eed72847500de266fea262ddc
Cristen Negron <cristy.negron@gmail.com>
2022-02-09 17:38:24 -0500

    views

--- capstone_django/capstone/views.py
+++ capstone_django/capstone/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CartSerializer, CategorySerializer, ProductSerializer
+from .models import Category, Product, Cart
 
-# Create your views here.
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CartList(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CartDetail(generics.RetrieveUpdateDestoryAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
