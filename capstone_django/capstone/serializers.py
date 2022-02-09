c1319ab1b139b84bb2fe80a8d59f4ee454d49f5e
Cristen Negron <cristy.negron@gmail.com>
2022-02-09 14:41:42 -0500

    edit serializeres

--- capstone_django/capstone/serializers.py
+++ capstone_django/capstone/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Product, Cart
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     products = serializers.HyperlinkedRelatedField(
@@ -11,3 +11,24 @@ class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'description', 'products')
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    categories = serializers.HyperlinkedRelatedField(
+        view_name='category_detail',
+        read_only=True
+    )
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price', 'descripton', 'category', 'image')
+
+class CartSerializer(serializers.HyperlinkedModelSerializer):
+    products = serializers.HyperlinkedRelatedField(
+        view_name='product_detail',
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Cart
+        fields = ('id', 'product', 'quantity', 'total')
