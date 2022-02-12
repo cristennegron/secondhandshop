fa7d4ace26f882f21910c5052568e8365843cbea
Cristen Negron <cristy.negron@gmail.com>
2022-02-12 00:43:32 -0500

    product list rendering

--- capstone_django/client/src/pages/Home.js
+++ capstone_django/client/src/pages/Home.js
@@ -1,26 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import axios from 'axios'
+import Product from '../components/Products'
 
 function Home() {
+    
     const [products, setProducts] = useState([])
 
     useEffect(() => {
-
         async function getProducts() {
-            const { data } = await axios.get('http://127.0.0.1:8000/api/products')
+            const { data } = await axios.get('http://localhost:8000/products/')
             setProducts(data)
         }
+        getProducts()
     }, [])
 
   return (
     <div>
         <h1>Latest Products</h1>
         <Row>
-
-            <Col sm={12} md={6} lg={4} xL={3}> 
-
+            {products.map(product => (
+              
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}> 
+                <Product product={product}/>
             </Col>
+              ))}
         </Row>
     </div>
   )
