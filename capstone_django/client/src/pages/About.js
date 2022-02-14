ced367e4d31e82c93d97458bccc67fc450662fe4
Cristen Negron <cristy.negron@gmail.com>
2022-02-14 13:40:23 -0500

    minor changes

--- capstone_django/client/src/pages/About.js
+++ capstone_django/client/src/pages/About.js
@@ -7,7 +7,7 @@ function About() {
        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <br>
       </br>
-      <h6>Website Developed by Cristen Negron</h6>
+      <h6>Developed by Cristen Negron</h6>
       <div className="list">
       <a href="https://github.com/cristennegron/">Github</a> | 
       <a href="https://www.linkedin.com/in/cristennegron/"> LinkedIn</a> |
