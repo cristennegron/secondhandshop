2dcf26795f5f58eb80980af6764686484519744a
Cristen Negron <cristy.negron@gmail.com>
2022-02-14 11:38:02 -0500

    cart functionality

--- capstone_django/client/src/pages/Cart.js
+++ capstone_django/client/src/pages/Cart.js
@@ -4,8 +4,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import { addToCart } from '../redux/cartActions'
 
-
-
 function Cart( {match} ) {
   
   const productId = match.params.id
@@ -22,23 +20,23 @@ function Cart( {match} ) {
   }, [dispatch, productId])
 
   return (
-    <Row>
-        <Col md={8}>
-          <h1> Shopping Cart </h1>
-          {cartItems.length === 0 ? (
 
-            <Link to='/'>Your cart is empty! Go Back</Link>
-          ) : (
-            <ListGroup variant='flush'>
+    <div>test</div>
+    // <Row>
+    //     <Col md={8}>
+    //       <h1> Shopping Cart </h1>
+    //       {cartItems.length === 0 ? (
 
-            </ListGroup>
-          )}
-        </Col>
-    </Row>
+    //         <Link to='/'>Your cart is empty! Go Back</Link>
+    //       ) : (
+    //         <ListGroup variant='flush'>
+
+    //         </ListGroup>
+    //       )}
+    //     </Col>
+    // </Row>
   )
-{/* <div>
-  test
-</div> */}
+
   
 
 }
--- capstone_django/client/src/redux/store.js
+++ capstone_django/client/src/redux/store.js
@@ -2,20 +2,20 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './productReducers'
-// import { cartReducer } from './cartReducer'
+import { cartReducer } from './cartReducer'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    // cart: cartReducer,
+    cart: cartReducer,
 })
 
-// const cartItemsFromStorage = localStorage.getItem('cartItems') ?
-//     JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+    JSON.parse(localStorage.getItem('cartItems')) : []
 
-const initialState= {}
-    // cart: {cartItems: cartItemsFromStorage}
-
+const initialState= {
+    cart: {cartItems: cartItemsFromStorage}
+}
 
 const middleware =  [thunk]
 
