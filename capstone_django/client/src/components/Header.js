9d90d3345a0e0c7a008becbf5ba09de88b32dc97
Cristen Negron <cristy.negron@gmail.com>
2022-02-13 21:00:39 -0500

    updated urls

--- capstone_django/client/src/components/Header.js
+++ capstone_django/client/src/components/Header.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
     <header>
         <Navbar bg="light" expand="lg">
-            <Container>
-            <Navbar.Brand href="/">secondhandshop</Navbar.Brand>
+          <Container>
+              {/* <LinkContainer to='/'> */}
+                <Navbar.Brand>secondhandshop</Navbar.Brand>
+              {/* </LinkContainer> */}
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i></Nav.Link>
-                
-            </Nav>
-            </Navbar.Collapse>
+                  <Nav className="me-auto">
+
+                {/* <LinkContainer to='/about'> */}
+                    <Nav.Link><i className="about"></i>About</Nav.Link>
+                {/* </LinkContainer> */}
+                    
+                {/* <LinkContainer to='/cart'> */}
+                    <Nav.Link><i className="cart"></i>Cart</Nav.Link>
+                {/* </LinkContainer>  */}
+                    
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
 
         </Navbar>   
--- capstone_django/client/src/pages/Product.js
+++ capstone_django/client/src/pages/Product.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Products from '../components/Products'
 
-function Product() {
+function Product( {match} ) {
+    const product = Products.find((p) => p._id == match.params.id)
     return (
         <div>
-
+            {product.name}
         </div>
     )
 }
