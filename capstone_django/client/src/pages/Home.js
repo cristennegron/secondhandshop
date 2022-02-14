d637e44c528569604787d541fe31d32cc7714104
Cristen Negron <cristy.negron@gmail.com>
2022-02-14 10:51:12 -0500

    reinstalled redux

--- capstone_django/client/src/pages/Home.js
+++ capstone_django/client/src/pages/Home.js
@@ -1,32 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
 import Product from '../components/Products'
 import { listProducts } from '../redux/productActions'
 
 function Home() {
     
-    // const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-    // useEffect(() => {
-    //   dispatch(listProducts())
+    useEffect(() => {
+      dispatch(listProducts())
 
-    // }, [])
+    }, [])
 
-    // const products = []
+    const products = []
 
   return (
     <div>
-        {/* <h1>Latest Products</h1>
+        <h1>Latest Products</h1>
         <Row>
             {products.map(product => (
               
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}> 
+            <Col key={product.id} sm={12} md={6} lg={4} xl={3}> 
                 <Product product={product}/>
             </Col>
               ))}
-        </Row> */}
+        </Row>
     </div>
   )
 }
